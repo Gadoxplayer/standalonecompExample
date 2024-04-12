a6579f8024d7415e37eeb60cbf1f7d3255a54580
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2024-04-12 17:24:35 -0500

    routing the stand alone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+
 import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [WelcomeComponent],
   standalone: true,
+  imports: [WelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
